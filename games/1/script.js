0a6b6224d1d24fed20494d6ce0422015068fc4e9
Azer Sadykhzadeh <51178055+sadykhzadeh@users.noreply.github.com>
2020-04-18 17:07:48 +0400

    Update: bug fixed.

--- games/1/script.js
+++ games/1/script.js
@@ -14,8 +14,8 @@ var selection = '';
 var balance;
 var bet;
 
-var recentCircs = [,,,,,,,,,];
-var recents     = [,,,,,,,,,];
+var recentCircs = [, , , , , , , , , ];
+var recents = [, , , , , , , , , ];
 var rCnt = 0;
 
 var greenHex = '#05AC54';
@@ -40,86 +40,80 @@ var winlose;
 
 var instructionPage;
 
-function allin()
-{
+function allin() {
 
-    if(balance > 0)
-    {
-    bet += balance;
-    balance = 0;
-    setBet();
+    if (balance > 0) {
+        bet += balance;
+        balance = 0;
+        setBet();
     }
 }
 
-function openRules()
-{
-    instructionPage.style.visibility='visible';
+function openRules() {
+    instructionPage.style.visibility = 'visible';
 }
 
-function onLoad()
-{
-    
-    balance = prompt("Укажите, сколько денег(виртуальных) у Вас будет в начале игры (500 - рекомендуемая сумма)","500");
+function onLoad() {
+
+    balance = prompt("Укажите, сколько денег(виртуальных) у Вас будет в начале игры (500 - рекомендуемая сумма)", "500");
     balance = parseInt(balance);
     winlose = document.getElementById('winlose');
     winnings = document.getElementById('winnings');
 
-    winnings.style.visibility ='hidden';
+    winnings.style.visibility = 'hidden';
     winlose.style.visibility = 'hidden';
 
-wb = document.getElementById('wheelback')
-wb.style.visibility = 'hidden';
-for(i=0;i< amntNew;i++)
-{    
-var slotParent = document.getElementsByTagName
-("tr");
- var newSlot = document.createElement('td');
- var newId = 's'+idCnt;
- slotId2[idCnt] = newId;
- newSlot.setAttribute('id', newId);
- newSlot.innerHTML = "new";
- slotparent.appendChild(newSlot);
- idCnt++;
-}
+    wb = document.getElementById('wheelback')
+    wb.style.visibility = 'hidden';
+    for (i = 0; i < amntNew; i++) {
+        var slotParent = document.getElementsByTagName("tr");
+        var newSlot = document.createElement('td');
+        var newId = 's' + idCnt;
+        slotId2[idCnt] = newId;
+        newSlot.setAttribute('id', newId);
+        newSlot.innerHTML = "new";
+        slotparent.appendChild(newSlot);
+        idCnt++;
+    }
 
 
-for(i=0;i < slotId2.length-1;i++){
-    wheelSlots[i] = document.getElementById(slotId2[i]);
-}
+    for (i = 0; i < slotId2.length - 1; i++) {
+        wheelSlots[i] = document.getElementById(slotId2[i]);
+    }
 
-wheel = document.getElementById('wheel');
-wheel.style.visibility = 'hidden';
+    wheel = document.getElementById('wheel');
+    wheel.style.visibility = 'hidden';
 
-recentCircs[0] = document.getElementById('c0');
-recentCircs[1] = document.getElementById('c1');
-recentCircs[2] = document.getElementById('c2');
-recentCircs[3] = document.getElementById('c3');
-recentCircs[4] = document.getElementById('c4');
-recentCircs[5] = document.getElementById('c5');
-recentCircs[6] = document.getElementById('c6');
-recentCircs[7] = document.getElementById('c7');
-recentCircs[8] = document.getElementById('c8');
-recentCircs[9] = document.getElementById('c9');
+    recentCircs[0] = document.getElementById('c0');
+    recentCircs[1] = document.getElementById('c1');
+    recentCircs[2] = document.getElementById('c2');
+    recentCircs[3] = document.getElementById('c3');
+    recentCircs[4] = document.getElementById('c4');
+    recentCircs[5] = document.getElementById('c5');
+    recentCircs[6] = document.getElementById('c6');
+    recentCircs[7] = document.getElementById('c7');
+    recentCircs[8] = document.getElementById('c8');
+    recentCircs[9] = document.getElementById('c9');
 
-instructionPage = document.getElementById('instructions');
+    instructionPage = document.getElementById('instructions');
 
-instructionPage.style.visibility='hidden';
+    instructionPage.style.visibility = 'hidden';
 
-redBtn = document.getElementById('redbtn');
-greenBtn = document.getElementById('greenbtn');
-blackBtn = document.getElementById('blackbtn');
+    redBtn = document.getElementById('redbtn');
+    greenBtn = document.getElementById('greenbtn');
+    blackBtn = document.getElementById('blackbtn');
 
-resultTxt = document.getElementById('result');
-balanceTxt = document.getElementById('money');
-//choiceTxt =document.getElementById('choice');
-//choiceTxt.style.visibility = 'hidden';
-betTxt = document.getElementById('wager');
+    resultTxt = document.getElementById('result');
+    balanceTxt = document.getElementById('money');
+    //choiceTxt =document.getElementById('choice');
+    //choiceTxt.style.visibility = 'hidden';
+    betTxt = document.getElementById('wager');
 
-//balance = 500;
-bet = 0;
+    //balance = 500;
+    bet = 0;
 
-setBet();
-//spin();
+    setBet();
+    //spin();
 }
 
 /*function spinAnim(){
@@ -143,63 +137,58 @@ setBet();
     
 }*/
 
-function closeWheel()
-{
+function closeWheel() {
     wheel.style.visibility = 'hidden';
     wb.style.visibility = 'hidden';
-    winnings.style.visibility ='hidden';
+    winnings.style.visibility = 'hidden';
     winlose.style.visibility = 'hidden';
     bet = 0;
     setBet();
-    
+
     //addMore();
-    
+
 }
 
-function winText()
-{
+function winText() {
     winnings.style.visibility = 'visible';
     winlose.style.visibility = 'visible';
-    
-    if(theWnr == selection){
+
+    if (theWnr == selection) {
         winlose.innerHTML = "Поздравляем! 😃";
-        if(theWnr == 'green'){
-        winnings.innerHTML = '+$' + (bet * 15);
-        }
-        else{
+        if (theWnr == 'green') {
+            winnings.innerHTML = '+$' + (bet * 15);
+        } else {
             winnings.innerHTML = '+$' + (bet * 2);
         }
-    }
-    else{
-        winlose.innerHTML ="Сочувствуем... 😞"
+    } else {
+        winlose.innerHTML = "Сочувствуем... 😞"
         winnings.innerHTML = '-$' + bet;
     }
 }
 
-function spin(){
-    
+function spin() {
+
     "use strict";
     wb.style.visibility = 'visible';
     wheel.style.visibility = 'visible';
     wheel.style.WebkitAnimationPlayState = "paused";
     //setTimeout(restartAnim, 2000);
-    
-    
+
+
     wheel.style.animation = 'true';
     wheel.offsetHeight;
     wheel.style.animation = null;
-    
+
     setTimeout(winText, 3500);
     setTimeout(closeWheel, 5000);
-    
-    
+
+
     //setTimeout(closeWheel, 3500);
-    
-    for(i = 0; i < wheelSlots.length; i++)
-    {
+
+    for (i = 0; i < wheelSlots.length; i++) {
         var randTemp = Math.floor(Math.random() * Math.floor(15));
         slotRand[i] = randTemp;
-        switch (randTemp){
+        switch (randTemp) {
             case 0:
             case 2:
             case 4:
@@ -223,31 +212,27 @@ function spin(){
                 break;
         }
     }
-    for(i = 0; i < wheelSlots.length; i++)
-    {
-     wheelSlots[i].innerHTML = slotRand[i] + 1;
+    for (i = 0; i < wheelSlots.length; i++) {
+        wheelSlots[i].innerHTML = slotRand[i] + 1;
     }
     wheelSlots[wheelSlots.length - landOffset].innerHTML = result + 1;
-    wheelSlots[wheelSlots.length -landOffset].style.backgroundColor = col;
+    wheelSlots[wheelSlots.length - landOffset].style.backgroundColor = col;
     wheelSlots[wheelSlots.length - landOffset].style.borderWidth = '6px';
-    
+
 }
 
-function closeInst()
-{
-    instructionPage.style.visibility='hidden';
+function closeInst() {
+    instructionPage.style.visibility = 'hidden';
 }
 
-function newRecent(color)
-{
+function newRecent(color) {
     //if(rCnt > 9){
-        
-    for(i = recents.length - 1; i > 0; i--)
-    {
-        recents[i] = recents[i-1];
-        recentCircs[i].innerHTML = recentCircs[i-1].innerHTML;
+
+    for (i = recents.length - 1; i > 0; i--) {
+        recents[i] = recents[i - 1];
+        recentCircs[i].innerHTML = recentCircs[i - 1].innerHTML;
         recentCircs[i].style.color = 'white';
-        
+
     }
     recents[0] = color;
     recentCircs[0].innerHTML = result + 1;
@@ -258,192 +243,163 @@ function newRecent(color)
     //recents[rCnt] = color;
     //rCnt++;
     //alert(recents + 'normal fill');
-        //alert(recents.length);
+    //alert(recents.length);
     //}
-    for(i = 0; i < recents.length ; i++)
-    {
-        if(recents[i] == 'green')
-        {
+    for (i = 0; i < recents.length; i++) {
+        if (recents[i] == 'green') {
             recentCircs[i].style.backgroundColor = greenHex;
-        }
-         else if(recents[i] == 'red')
-        {
+        } else if (recents[i] == 'red') {
             recentCircs[i].style.backgroundColor = 'red';
-        }
-         else if(recents[i] == 'black')
-        {
+        } else if (recents[i] == 'black') {
             recentCircs[i].style.backgroundColor = 'black';
         }
-        
-        
+
+
     }
-    
+
 }
 
-function choose(color)
-{
-    switch(color)
-    {
-    case 'red':
-        selection = 'red';
-        //redBtn.style.fontSize = '20px';
-        //blackBtn.style.fontSize = '12px';
-        //greenBtn.style.fontSize = '12px';
-        redBtn.style.borderWidth = '4px';
-        blackBtn.style.borderWidth = '1px';
-        greenBtn.style.borderWidth = '1px';
-        break;
-    case 'black':
-        selection = 'black';
-        //redBtn.style.fontSize = '12px';
-        //blackBtn.style.fontSize = '20px';
-        //greenBtn.style.fontSize = '12px';
-        redBtn.style.borderWidth = '1px';
-        blackBtn.style.borderWidth = '4px';
-        greenBtn.style.borderWidth = '1px';
-        break;
-    case 'green':
-        selection = 'green';
-        //redBtn.style.fontSize = '12px';
-        //blackBtn.style.fontSize = '12px';
-        //greenBtn.style.fontSize = '20px';
-        redBtn.style.borderWidth = '1px';
-        blackBtn.style.borderWidth = '1px';
-        greenBtn.style.borderWidth = '4px';
-        break;
+function choose(color) {
+    switch (color) {
+        case 'red':
+            selection = 'red';
+            //redBtn.style.fontSize = '20px';
+            //blackBtn.style.fontSize = '12px';
+            //greenBtn.style.fontSize = '12px';
+            redBtn.style.borderWidth = '4px';
+            blackBtn.style.borderWidth = '1px';
+            greenBtn.style.borderWidth = '1px';
+            break;
+        case 'black':
+            selection = 'black';
+            //redBtn.style.fontSize = '12px';
+            //blackBtn.style.fontSize = '20px';
+            //greenBtn.style.fontSize = '12px';
+            redBtn.style.borderWidth = '1px';
+            blackBtn.style.borderWidth = '4px';
+            greenBtn.style.borderWidth = '1px';
+            break;
+        case 'green':
+            selection = 'green';
+            //redBtn.style.fontSize = '12px';
+            //blackBtn.style.fontSize = '12px';
+            //greenBtn.style.fontSize = '20px';
+            redBtn.style.borderWidth = '1px';
+            blackBtn.style.borderWidth = '1px';
+            greenBtn.style.borderWidth = '4px';
+            break;
     }
     //choiceTxt.innerHTML = 'Color: ' + selection;
 }
 
-function changeBet(change)
-{
+function changeBet(change) {
 
     addMore();
-    if(change < 0)
-    {
-        if(bet >= Math.abs(change))
-        {
+    if (change < 0) {
+        if (bet >= Math.abs(change)) {
             bet += change;
             balance -= change;
             setBet();
         }
-    }
-    else if(balance >= change)
-    {
+    } else if (balance >= change) {
         bet += change;
         balance -= change;
         setBet();
     }
 }
 
-function addMore(){
-    if(bet <= 0 && balance <= 0){
-    alert ("Вы банкрот 😞")
-    balance = prompt("Хотите пополнить свой баланс?", "500");
-    balance = parseInt(balance);
-}
-setBet();
-}
-
-function placeBet()
-{
-
-addMore();
-
-if(selection == '')
-{
-alert('Вам нужно указать цвет!');
-return;
-}
-else{
-    if(balance == 0 && bet == 0)
-    {
-        alert("У вас нет денег!");
+function addMore() {
+    if (bet <= 0 && balance <= 0) {
+        alert("Вы банкрот 😞")
+        balance = prompt("Хотите пополнить свой баланс?", "500");
+        balance = parseInt(balance);
     }
-else
-{
+    setBet();
+}
 
-if(bet > 0)
-{
-    getRand(15);
-    
-    switch(result)
-    {
-        case 0:
-        case 2:
-        case 4:
-        case 6:
-        case 8:
-        case 10:
-        case 12:
-            win('black');
-            col = 'black';
-            newRecent('black');
-            fnt = 'white';
-            break;
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 9:
-        case 11:
-        case 13:
-            win('red');
-            col = 'red';
-            newRecent('red');
-            fnt = 'white';
-            break;
-        case 14:
-            win('green');
-            col = greenHex;
-            newRecent('green');
-            fnt = 'black';
-            break;
+function placeBet() {
+
+    addMore();
+
+    if (selection == '') {
+        alert('Вам нужно указать цвет!');
+        return;
+    } else {
+        if (balance == 0 && bet == 0) {
+            alert("У вас нет денег!");
+        } else {
+
+            if (bet > 0) {
+                getRand(15);
+
+                switch (result) {
+                    case 0:
+                    case 2:
+                    case 4:
+                    case 6:
+                    case 8:
+                    case 10:
+                    case 12:
+                        win('black');
+                        col = 'black';
+                        newRecent('black');
+                        fnt = 'white';
+                        break;
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 9:
+                    case 11:
+                    case 13:
+                        win('red');
+                        col = 'red';
+                        newRecent('red');
+                        fnt = 'white';
+                        break;
+                    case 14:
+                        win('green');
+                        col = greenHex;
+                        newRecent('green');
+                        fnt = 'black';
+                        break;
+                }
+                spin();
+            }
+
+        }
     }
-    spin();
-}        
-    
-}
-}
 
 }
 
-function win(winner)
-{
+function win(winner) {
     theWnr = winner;
     resultTxt.innerHTML = 'Result: ' + winner;
-    if(selection == 'red' && winner == 'red')
-    {
+    if (selection == 'red' && winner == 'red') {
         balance += (bet * 2);
-        
-    } 
-    else if(selection == 'black' && winner == 'black')
-    {
+
+    } else if (selection == 'black' && winner == 'black') {
         balance += (bet * 2);
-        
-    }
-    else if(selection == 'green' && winner == 'green')
-    {
+
+    } else if (selection == 'green' && winner == 'green') {
         balance += ((bet * 15));
-        
-    }
-    else{
+
+    } else {
         setBet();
     }
-    
+
     setBal();
 }
 
-function setBal()
-{
+function setBal() {
     balanceTxt.innerHTML = '$' + balance;
 }
 
-function setBet()
-{
+function setBet() {
     betTxt.innerHTML = '$' + bet;
     setBal();
 }
-function getRand(max){
+
+function getRand(max) {
     result = Math.floor(Math.random() * Math.floor(max));
 }
--- reaction_test_script.js
+++ reaction_test_script.js
@@ -8,39 +8,39 @@ var best;
 var worst;
 var average;
 var scores = [];
-window.onload = function(){
-        tester = document.getElementById("tester");
-        starter = document.getElementById("starter");
-        tester.onclick = test;
+window.onload = function() {
+    tester = document.getElementById("tester");
+    starter = document.getElementById("starter");
+    tester.onclick = test;
 }
-function test(){
-    if(tester.style.backgroundColor=="rgb(255, 0, 0)"){
+
+function test() {
+    if (tester.style.backgroundColor == "rgb(255, 0, 0)") {
         scores.push(result);
         var sum = 0;
         var b = true;
         var w = true;
-        for(var v=0; v<scores.length; v++){
-            if(result>scores[v]){
+        for (var v = 0; v < scores.length; v++) {
+            if (result > scores[v]) {
                 b = false;
             }
-            if(result<scores[v]){
+            if (result < scores[v]) {
                 w = false;
             }
             sum += scores[v];
         }
-        if(b){
+        if (b) {
             best = result;
         }
-        if(w){
+        if (w) {
             worst = result;
         }
-        average = sum/scores.length;
-        alert("Ваше текущее время реакции: "+result+"ms\nЛучшее: "+best+"ms\nХудшее: "+worst+"ms\nСреднее арифметическое всех Ваших реакций: "+average+"ms");
+        average = sum / scores.length;
+        alert("Ваше текущее время реакции: " + result + "ms\nЛучшее: " + best + "ms\nХудшее: " + worst + "ms\nСреднее арифметическое всех Ваших реакций: " + average + "ms");
         result = 0;
         clearInterval(interval);
         tester.style.backgroundColor = "#0F0";
-    }
-    else if(tester.style.backgroundColor=="rgb(0, 255, 0)"){
+    } else if (tester.style.backgroundColor == "rgb(0, 255, 0)") {
         //Если нажал на зелёную кнопку
         alert("Фальстарт!");
         clearTimeout(timeout);
@@ -48,17 +48,19 @@ function test(){
     }
     starter.style.display = "block";
 }
-function start(){
+
+function start() {
     tester.style.backgroundColor = "#0F0";
     starter.style.display = "none";
     //рандомайзер
-    time = Math.floor(Math.random()*10000+5000);
+    time = Math.floor(Math.random() * 10000 + 5000);
     timeout = setTimeout(change, time);
 }
-function change(){
+
+function change() {
     tester.style.backgroundColor = "#F00";
-    interval = setInterval(function(){
+    interval = setInterval(function() {
         result += 1;
-       // tester.innerHTML = result;
+        // tester.innerHTML = result;
     }, 1);
 }
